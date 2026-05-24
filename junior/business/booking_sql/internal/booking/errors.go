cd4c3311edc9225e89b0211cc782ea37fa7d6e1d
Alice088 <alien280579@gmail.com>
2026-05-24 12:32:21 +0300

    feat(ErrTimeAlreadyTaken): add implement of HTTPFriendly
    add:
    - New() init
    
    Fix:
    - Make values not importable

--- junior/business/booking_sql/internal/booking/errors.go
+++ junior/business/booking_sql/internal/booking/errors.go
@@ -1,19 +1,36 @@
 package booking
 
-import "errors"
+import (
+	"errors"
+	"net/http"
+)
 
 var (
 	ErrRoomNameTooLong = errors.New("room name too long")
 )
 
 type ErrTimeAlreadyTaken struct {
-	Msg string
+	msg    string
+	fMsg   string
+	status int
+}
+
+func NewErrTimeAlreadyTaken() error {
+	return &ErrTimeAlreadyTaken{
+		msg:    "time have already taken",
+		fMsg:   "time have already taken",
+		status: http.StatusConflict,
+	}
 }
 
 func (e *ErrTimeAlreadyTaken) Error() string {
-	return e.Msg
+	return e.msg
 }
 
 func (e *ErrTimeAlreadyTaken) FMsg() string {
-	return e.Msg
+	return e.fMsg
+}
+
+func (e *ErrTimeAlreadyTaken) Status() int {
+	return e.status
 }
